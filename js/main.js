730e6f8c795adf4ad3002ede263887adfb27157b
Nicolò Pincelli <pincelli.n@gmail.com>
2024-06-24 14:49:44 +0200

    end

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const {createApp} = Vue;
+ 
+createApp({
+    data(){
+        return {
+          text: "Scoprendo Vue",
+          imgSrc: "img/tk5xdp3nv40a1.jpg",
+          ImgClass:"size"
+        };
+    }
+}).mount('#container');
